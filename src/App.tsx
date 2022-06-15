7c348cf7432db867f8ec84e6ca50a51649062a30
koyo.takaya <koyo7jp@gmail.com>
2022-06-15 14:15:20 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,28 @@
 import './App.css';
-import Amplify from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import { AccountLinkage } from './components/link/AccountLinkage';
-
+import { Authenticator } from '@aws-amplify/ui-react';
+import { I18n } from 'aws-amplify';
+import { translations } from '@aws-amplify/ui-react';
+import Amplify from 'aws-amplify';
 import awsExports from "./aws-exports";
+
+I18n.putVocabularies(translations);
+I18n.setLanguage('ja');
+
 Amplify.configure(awsExports);
 
 function App() {
   return (
     <>
-      <Authenticator>
+      <Authenticator
+        hideSignUp={true}
+        className="pt-20"
+        >
         {({ signOut, user }) => (
           <div className="App">
               <AccountLinkage></AccountLinkage>
-            <button onClick={signOut}>Sign out</button>
+            <button onClick={signOut}>ログアウト</button>
           </div>
         )}
       </Authenticator>
